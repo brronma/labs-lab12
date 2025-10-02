3efd3df9f16f662c742c776102ee05aa5bd12e31
Biruk gessesse <TEKstudent@vlab.lan>
2025-10-02 11:31:03 -0700

    version change to 1.13.1

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ module "app_security_group" {
 
 module "lb_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/web"
-  version = "5.3.8"
+  version = "1.13.1"
 
   name        = "lb-sg-project-alpha-dev"
   description = "Security group for load balancer with HTTP ports open within VPC"
