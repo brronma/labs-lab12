b1434335fb73ceb2dabf5fbcff3cd622432b6b66
Biruk gessesse <TEKstudent@vlab.lan>
2025-10-02 13:39:29 -0700

    version correction

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ module "app_security_group" {
 
 module "lb_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/web"
-  version = "3.17,0"
+  version = "5.3.0"
 
   name        = "lb-sg-project-alpha-dev"
   description = "Security group for load balancer with HTTP ports open within VPC"
